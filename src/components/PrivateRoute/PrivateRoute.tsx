29a03882bba6045c7b78d8e2afdd808775188dd0
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2022-11-03 21:46:16 -0300

    feat: private route

---
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -0,0 +1,17 @@
+import React, { useContext } from "react"
+import { Navigate, Route, RouteProps } from "react-router-dom"
+import { routesPath } from "../../constants/routes"
+import { AuthContext, TAuthContext } from "../../Context/AuthContext"
+type PrivateRouteProps = RouteProps
+
+export const PrivateRoute = (props: PrivateRouteProps) => {
+  const { authenticated } = React.useContext(AuthContext) as TAuthContext
+
+  if (!authenticated)
+    return (
+      <>
+        <Navigate to={routesPath.singIn} replace={true} />
+      </>
+    )
+  return <>{props.children}</>
+}
