57cda7c2a96f572c3dd818f88a0dc2a8c891cf26
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2022-11-05 13:26:28 -0300

    feat: input error

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,20 +1,23 @@
 import React, { FormEvent, InputHTMLAttributes } from "react"
 import Label from "../Label/Label"
+import { P } from "../Typography/P/P"
 import * as S from "./styles"
-type InputProps = {
+export type InputProps = {
   label?: string
   stylesWrapper?: React.CSSProperties
   icon?: React.ReactNode
+  error?: any
 } & InputHTMLAttributes<HTMLInputElement>
 
-export const Input = ({ label, stylesWrapper, icon, ...props }: InputProps) => {
+export const Input = ({ label, stylesWrapper, icon, error, ...props }: InputProps) => {
   return (
-    <S.Wrapper style={stylesWrapper}>
+    <S.Wrapper style={stylesWrapper} >
       {label && <Label children={label} />}
-      <S.Input>
+      <S.Input error={error}>
         {icon && icon}
         <input {...props} />
       </S.Input>
+      {error && <P style={{color: '#ff4949'}}>Opss... Algo de errado por aqui</P>}
     </S.Wrapper>
   )
 }
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,49 +1,52 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
+import { InputProps } from "./Input"
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1.2rem;
 `
-export const Input = styled.div`
-  border-radius: 0.4rem;
-  border: 0.1rem solid #979797;
-  padding: 1.2rem;
-  display: flex;
-  align-items: center;
-  gap: 1.2rem;
-  input {
-    font-size: 1.6rem;
-    -webkit-appearance: none;
-    box-shadow: none !important;
-    width: 100%;
-    outline: none;
-    background-color: transparent;
-    border-color: transparent;
-    color: #ffffff;
-    &::placeholder {
-      /* Chrome, Firefox, Opera, Safari 10.1+ */
-      color: #535353;
-      opacity: 1; /* Firefox */
+export const Input = styled.div<Pick<InputProps, "error">>`
+  ${({ error }) => css`
+    border-radius: 0.4rem;
+    border: 0.1rem solid ${error ? "#ff4949" : "#979797"};
+    padding: 1.2rem;
+    display: flex;
+    align-items: center;
+    gap: 1.2rem;
+    input {
+      font-size: 1.6rem;
+      -webkit-appearance: none;
+      box-shadow: none !important;
+      width: 100%;
+      outline: none;
+      background-color: transparent;
+      border-color: transparent;
+      color: #ffffff;
+      &::placeholder {
+        /* Chrome, Firefox, Opera, Safari 10.1+ */
+        color: #535353;
+        opacity: 1; /* Firefox */
+      }
+
+      &:-ms-input-placeholder {
+        /* Internet Explorer 10-11 */
+        color: #535353;
+      }
+
+      &::-ms-input-placeholder {
+        /* Microsoft Edge */
+        color: #535353;
+      }
     }
 
-    &:-ms-input-placeholder {
-      /* Internet Explorer 10-11 */
-      color: #535353;
+    svg {
+      height: 2.4rem;
+      width: 2.4rem;
     }
 
-    &::-ms-input-placeholder {
-      /* Microsoft Edge */
-      color: #535353;
+    a {
+      text-decoration: none;
     }
-  }
-
-  svg {
-    height: 2.4rem;
-    width: 2.4rem;
-  }
-
-  a {
-    text-decoration: none;
-  }
+  `}
 `
