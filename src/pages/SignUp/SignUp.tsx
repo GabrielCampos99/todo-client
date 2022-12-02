414c487c27a04533f53835113f3c6a168e0e3491
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2022-12-01 22:52:10 -0300

    refactor user creator

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,13 +1,15 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import { Button } from "../../components/Button/Button"
 import { Header } from "../../components/Header/Header.form"
 import { Input } from "../../components/Input/Input"
+import { Spinner } from "../../components/Spinner/Spinner"
 import { H1 } from "../../components/Typography/H1/H1"
 import { routesPath } from "../../constants/routes"
 import { TToastContext, useToast } from "../../Context/ToastContext"
 import { useAxios } from "../../hooks/Axios/useAxios"
+import { CreateUserService } from "../../services/signUp/CreateUserService"
 
 type Props = {}
 
@@ -19,45 +21,28 @@ type SignUpRef = {
 
 export const SignUp = (props: Props) => {
   const singUpRef = useRef<SignUpRef>({ email: "", password: "", name: "" })
-  const { error, loading, response, fetchData } = useAxios<any, any>()
   const toast = useToast() as TToastContext
   const navigate = useNavigate()
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
 
   const handleForm = (event: React.ChangeEvent<HTMLInputElement>, name: "email" | "password" | "name") => {
     const value = event.target.value
     singUpRef.current[name] = value
-    console.log(singUpRef.current, "loginRef.current")
   }
 
   const handleSignUp = async (body: any) => {
+    setIsLoading(true)
+    const res = await CreateUserService(body)
+    setIsLoading(false)
+    if (!res) return toast.contextValue.open(`Erro ao criar Usuario`)
+    navigate(`${routesPath.singIn}`)
 
-    await fetchData({
-      method: "POST",
-      url: "/users",
-      headers: {
-        accept: "*/*",
-      },
-      data: body,
-    })
   }
-
-  useEffect(() => {
-    if (!!response && !error) {
-      toast.contextValue.open(`Usuario criado com sucesso`)
-      navigate(`${routesPath.singIn}`)
-    }
-
-    if (!response && !!error) toast.contextValue.open(`${error.response.data.message}`)
-  }, [response, error])
-
-  useEffect(() => {
-    console.log(error, "error")
-    console.log(loading, "loading")
-    console.log(response, "response")
-  }, [error, loading, response])
 
   return (
     <Wrapper>
+      {isLoading && <Spinner />}
       <Header path="/sign-in" />
       <H1 style={{ marginTop: "4rem" }}>Cadastro</H1>
 
